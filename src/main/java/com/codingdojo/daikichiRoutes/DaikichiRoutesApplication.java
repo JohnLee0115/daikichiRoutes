5cd12f9a1455212854ea53ede52e900345200f57
JohnLee0115 <johnlee011500@gmail.com>
2023-08-09 20:07:05 -0700

    Daikichi Path Variables

--- src/main/java/com/codingdojo/daikichiRoutes/DaikichiRoutesApplication.java
+++ src/main/java/com/codingdojo/daikichiRoutes/DaikichiRoutesApplication.java
@@ -1,12 +1,10 @@
 package com.codingdojo.daikichiRoutes;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-@SpringBootApplication
 
-@RestController
+@SpringBootApplication
 public class DaikichiRoutesApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/codingdojo/daikichiRoutes/HomeController.java
+++ src/main/java/com/codingdojo/daikichiRoutes/HomeController.java
@@ -1,5 +1,6 @@
 package com.codingdojo.daikichiRoutes;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +21,21 @@ public class HomeController {
 	    @RequestMapping("/tomorrow")
 	    public String tomorrow() {
 	    	return "Tomorrow, an opportunity will arise, so be sure to be open to new ideas!";
+	    }
+	    
+	    @RequestMapping("/travel/{destination}")
+	    public String travel(@PathVariable("destination") String destination) {
+	    	return "Congratulations! You will soon travel to " + destination;
+	    }
+	    
+	    @RequestMapping("lotto/{number}")
+	    public String lotto(@PathVariable("number") int number) {
+	    	if (number % 2 == 0) {
+	    		return "You will take a grand journey in the near furture, but be wary of tempting offers";
+	    	} else {
+	    		return "You have enjoyed the fruits of your labor but now is a great time to spend time with family and friends";
+	    	}
+	    	
 	    }
     
 
